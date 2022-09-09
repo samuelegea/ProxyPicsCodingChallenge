9ea0a1e48c73378b55d6e4ac50ead42a4fb62c85
Samuel Egea <samuelegea@hotmail.com>
2022-09-09 14:43:13 -0300

    ⚙ Added models

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :orders
+  devise_for :users
+  resources :addresses
+  get 'static_pages/homepage'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
